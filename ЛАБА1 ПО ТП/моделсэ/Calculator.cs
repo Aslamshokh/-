a0b30bf712f2c4907c844312c4cfb26d2b23685c
Aslamshokh <167721871+Aslamshokh@users.noreply.github.com>
2025-05-05 01:34:22 +0400

    Add files via upload

---
+++ ЛАБА1 ПО ТП/моделсэ/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+//sbyte, decimal, RadioButton,  _ViewStart.cshtml, Compare, ValidationSummary(), состояние сеанса (класс Session), IndexOf, Remove(), Insert(). 
+namespace Calculator.Models
+
+{
+    public class Calculator
+    {
+        [Required(ErrorMessage ="Value is invalid!")]
+        public decimal operand1 { get; set; }
+        //[Compare("operand1", ErrorMessage ="Operands must not be equal!")]
+        [Required(ErrorMessage = "Value is invalid!")]
+        public sbyte operand2 { get; set; }
+        public decimal result { get; set; }
+        public string operation { get; set; }
+    }
+}
